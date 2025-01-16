ac824d899f21313c825e65bbc0ee43c2fd811fa0
Michael Sumner <mdsumner@gmail.com>
2025-01-17 09:34:43 +1100

    link

--- Makefile
+++
@@ -1,4 +0,0 @@
-FlawedExample.html: original/index.html
-	Rscript -e "source('redirect.R')"
-
-
--- redirect.R
+++
@@ -1,7 +0,0 @@
-x <- readLines("original/index.html")
-
-y <- gsub("bl.ocks.org", "https://blocks.roadtolarissa.com", x)
-
-writeLines(y, "FlawedExample.html")
-
-
